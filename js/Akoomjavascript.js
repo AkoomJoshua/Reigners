fa086752e5b4d3cb5da760538aa4fac71972f340
AKOOM Terna Joshua <sandiego2643@gmail.com>
2021-05-12 14:26:34 +0100

    Company name was changed, new files were added and changes were made to the sub-pages

--- js/Akoomjavascript.js
+++ js/Akoomjavascript.js
@@ -1,2 +1,26 @@
-/** multiple line comment
-// single line cooment
+	var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  slides[slideIndex-1].style.display = "block";
+  setTimeout(showSlides, 5000); // Change image every 2 seconds
+}
+
+const navToggle = document.querySelector('#navToggle');
+const nav = document.querySelector('nav');
+const navIcon = document.querySelectorAll('#hamburg-icon');
+
+
+navToggle.addEventListener("click", () =>{
+	nav.classList.toggle('open');
+	navIcon.forEach(icon =>{
+		alert(page)
+	})
+})
